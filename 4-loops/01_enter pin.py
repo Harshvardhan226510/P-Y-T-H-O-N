6e4e00e8e3365282c0a060509a627a773e42c261
Harshvardhan226510 <22harshavardhan22@gmail.com>
2025-04-19 23:34:49 +0530

    01_enter pin.py

---
+++ 4-loops/01_enter pin.py
@@ -0,0 +1,9 @@
+print("BANK OF INDIA ")
+
+pin=int(input('Ente your pin: '))
+
+while pin !=2221 :
+  pin=int(input('Incorrect pin .\nPlease enter your pin again : '))
+
+if pin == 2221:
+  print('Correct pin') 
